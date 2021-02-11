1f82b761caec9aab10626550d6316051d7b0fec9
shashankkatte <shashankanand.katte@gmail.com>
2021-02-10 21:35:36 -0500

    src : Initial app scaffoldinga nd cleanup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Yo!</h1>
+      {/* Header */}
+      {/* tinder cards */}
+      {/* Swipe buttons */}
+    </div>
+  );
+}
+
+export default App;
