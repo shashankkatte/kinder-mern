d62d94cf7ec52b482a8e079f0515217b4be5447e
shashankkatte <shashankanand.katte@gmail.com>
2021-02-13 11:19:03 -0500

    Src : update Axios base URL to Heroku Kinder-backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8001/',
+  baseURL: 'https://kinder-backend.herokuapp.com',
 });
 
 export default instance;
