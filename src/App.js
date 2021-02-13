5e58baba91ac2f3858e16ee479f99ba7cab0c154
shashankkatte <shashankanand.katte@gmail.com>
2021-02-13 12:46:37 -0500

    Style : Add title component
    Add a title component and fix some sty;

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Header from './Header';
 import KinderCards from './KinderCards';
 import SwipeButtons from './SwipeButtons';
+import Title from './Title'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Title />
       <KinderCards />
      <SwipeButtons />
     </div>
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="title">
+      <p>Connecting Pet Lovers to Sheltered Furry Friends</p>
+    </div>
+  );
+}
+
+export default Title;
